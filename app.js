d804a4b9082c495adefb173f72a8e4c5f60cbb90
Linda Allen <lindaallenx@gmail.com>
2017-07-22 23:04:01 -0400

    added store and text var

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ var readable = {
 	'1': 'Paper',
 	'2': 'Scissors',
 }
+
+var cpuChoice = {
+	init: function() {
+		this.store = Math.floor(Math.random() * 3);
+		this.text = readable[this.store];
+	},
+	store:'' ,
+	text: '',
+};
+
+cpuChoice.init();
+console.log('cpuchoice:', cpuchoice.store, cpuchoice.text);
